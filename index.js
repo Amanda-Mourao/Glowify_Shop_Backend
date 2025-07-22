ce94b1ddcb71aef52e4f0a55c45f52c7d734a049
Amanda Mourao <a-mourao@web.de>
2025-07-22 19:09:12 +0200

    final structure

--- index.js
+++ index.js
@@ -1,9 +1,7 @@
 import express from "express";
 import cors from "cors";
 import dotenv from "dotenv";
-
 dotenv.config();
-
 import "./db/index.js";
 import "./db/associations.js";
 import userRouter from "./routers/user.router.js";
@@ -11,24 +9,30 @@ import productRouter from "./routers/product.router.js";
 import orderRouter from "./routers/order.router.js";
 import categoryRouter from "./routers/category.router.js";
 import errorHandler from "./middlewares/errorHandler.js";
-
 const app = express();
 const port = process.env.PORT || 8000;
-
+const allowedOrigins = [
+  "http://localhost:5173",
+  process.env.FRONTEND_URL || "https://glowify-shop-frontend.onrender.com",
+];
 app.use(
   cors({
-    origin: "http://localhost:5173",
+    origin: function (origin, callback) {
+      if (!origin) return callback(null, true);
+      if (allowedOrigins.indexOf(origin) !== -1) {
+        callback(null, true);
+      } else {
+        console.log("CORS blocked origin:", origin);
+        callback(new Error("Not allowed by CORS"));
+      }
+    },
     credentials: true,
   })
 );
-
 app.use(express.json());
-
 app.use("/users", userRouter);
 app.use("/products", productRouter);
 app.use("/orders", orderRouter);
 app.use("/category", categoryRouter);
-
 app.use(errorHandler);
-
 app.listen(port, () => console.log(`Server is running on port ${port}`));
